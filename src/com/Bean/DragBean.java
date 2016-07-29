ea28e9c1af0393e972fdbe6734f162229c6f112e
Vikrant Choudhary <vikrantchoudharylpu@gmail.com>
2016-07-30 05:13:15 +0530

    Add files via upload

---
+++ src/com/Bean/DragBean.java
@@ -0,0 +1,23 @@
+package com.Bean;
+
+public class DragBean {
+
+	float xpos;
+	float ypos;
+	public float getXpos() {
+		return xpos;
+	}
+	public void setXpos(float xpos) {
+		this.xpos = xpos;
+	}
+	public float getYpos() {
+		return ypos;
+	}
+	public void setYpos(float ypos) {
+		this.ypos = ypos;
+	}
+	
+
+	
+	
+}
